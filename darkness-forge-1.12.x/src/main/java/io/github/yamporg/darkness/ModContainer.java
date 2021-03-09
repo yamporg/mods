4de2bb8db4a6b51eda080d36639674f8426abbd4
Ivan Trubach <mr.trubach@icloud.com>
2021-03-09 21:29:34 +0300

    Look for metadata in all mcmod.info’s on classpath

--- darkness-forge-1.12.x/src/main/java/io/github/yamporg/darkness/ModContainer.java
+++ darkness-forge-1.12.x/src/main/java/io/github/yamporg/darkness/ModContainer.java
@@ -2,31 +2,21 @@ package io.github.yamporg.darkness;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
-import java.io.InputStream;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.DummyModContainer;
 import net.minecraftforge.fml.common.LoadController;
-import net.minecraftforge.fml.common.MetadataCollection;
-import net.minecraftforge.fml.common.ModMetadata;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public final class ModContainer extends DummyModContainer {
     public static final String MOD_ID = "darkness";
     public static final String MOD_NAME = "Darkness";
-    public static final ModMetadata MOD_METADATA;
-
-    static {
-        InputStream is = ModContainer.class.getResourceAsStream("/mcmod.info");
-        MetadataCollection mc = MetadataCollection.from(is, MOD_ID);
-        MOD_METADATA = mc.getMetadataForId(MOD_ID, null);
-    }
 
     public ModContainer() {
-        super(MOD_METADATA);
+        super(Workarounds.parseMetadata());
     }
 
     @Override
@@ -42,7 +32,7 @@ public final class ModContainer extends DummyModContainer {
 
     @Subscribe
     public void onModConstruction(FMLConstructionEvent event) {
-        ModConfig.inject();
+        Workarounds.injectConfig();
         ConfigManager.sync(MOD_ID, Config.Type.INSTANCE);
         MinecraftForge.EVENT_BUS.register(this);
     }
